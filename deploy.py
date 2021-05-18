3f6af7b339a69dfc2dcd3eeae39a8e49b8af0e7a
jojoconverteo <jonathan.ndamba@converteo.com>
2021-05-18 17:15:15 +0200

    correction deploy.py

--- deploy.py
+++ deploy.py
@@ -4,7 +4,7 @@ def deploy_streamlit_app():
     step_1 = "gcloud auth login"
     step_2 = "gcloud init"
     step_3 = "gcloud app deploy"
-    step_4 = "gcloud app browser"
+    step_4 = "gcloud app browse"
     all_step = [step_1, step_2, step_3, step_4]
     for step in all_step:
         os.system(step)
